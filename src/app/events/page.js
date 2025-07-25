e845b66d900fa0a8747bf70290f153892ea405c0
Titch-dev <lewis.d.perry@gmail.com>
2025-07-25 10:46:59 +0200

    mobile hidden eventdescription

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -10,12 +10,27 @@ const todaysDate = "2025-04-29T00:00:00Z";
 
 export default function EventsPage() {
 
+  const eventDateParser = (date) => {
+    
+    const eventDate = new Date(date);
+
+    const dateFormatted = new Intl.DateTimeFormat("en-GB", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        timeZone: "UTC",
+    }).format(eventDate);
+    
+    return dateFormatted;
+  }
+
   return (
     <div className={styles.wrapper}>
       <h1 className={`${rubikFont.className} ${styles.page_title}`}>Our Events</h1>
-      <section className={styles.event_list_wrapper}>
+      <section className={styles.event_wrapper}>
           {events.map((event, index) => (
-              <div key={event.id} className={styles.event_wrapper}>
+              <div key={event.id} className={styles.event_item_wrapper}>
+                <h2 className={styles.event_date}>{eventDateParser(event.start)}:</h2>
                 <EventItem key={event.id} props={event} isReversed={index % 2 === 1}/>
               </div>
           ))}
--- src/components/events/event-item.js
+++ src/components/events/event-item.js
@@ -1,46 +1,82 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import useMediaQuery from '@/hooks/useMediaQuery';
+
 import EventIcons from './event-icons';
 
 import styles from './event-item.module.css';
 
 import TornBackgroundSVG from '@/components/assets/patterns/torn-background-svg';
 import TornBorder from '@/components/assets/patterns/torn-border';
+import ChevronDownSVG from '../assets/icons/chevron-down-svg';
+
 import { colors } from '@/lib/colors';
 import { rubikFont } from '@/lib/fonts';
 
 
 export default function EventItem({ props, isReversed }) {
-    const { id, title, description, start, end, price, img, alt} = props;
+  const isMobile = useMediaQuery('(max-width: 767px)');
+  const [showDescription, setShowDescription] = useState(false);
+  const { id, title, description, start, end, price, img, alt} = props;
 
-    const startDate = new Date(start);
-    const endDate = new Date(end);
+  const [formattedDate, setFormattedDate] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
 
-    const eventDate = new Intl.DateTimeFormat("en-GB", {
-        weekday: "short",
-        day: "2-digit",
-        month: "short",
-        timeZone: "UTC",
-    }).format(startDate);
-    
-    const options = {
-        hour: "numeric",
-        hour12: true,
-        timeZone: "UTC"
-    }
 
-    const startTime = startDate.toLocaleTimeString("en-GB", options).toLowerCase();
-    const endTime = endDate.toLocaleTimeString("en-GB", options).toLowerCase();
+  useEffect(() => {
+      const startDate = new Date(start);
+      const endDate = new Date(end);
 
-    const eventMeta = { date: eventDate, price: price, start: startTime, end: endTime};
+      const date = new Intl.DateTimeFormat('en-GB', {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short',
+        timeZone: 'UTC',
+      }).format(startDate);
+
+      const options = { hour: 'numeric', hour12: true, timeZone: 'UTC' };
+      const startT = startDate.toLocaleTimeString('en-GB', options).toLowerCase();
+      const endT = endDate.toLocaleTimeString('en-GB', options).toLowerCase();
+
+      setFormattedDate(date);
+      setStartTime(startT);
+      setEndTime(endT);
+    }, [start, end]);
+
+  const handleDescriptionShow = () =>{
+    setShowDescription(prev => !prev);
+  }
+
+  const eventMeta = { date: formattedDate, price: price, start: startTime, end: endTime };
 
   return (
     <div className={`${styles.container} ${isReversed ? styles.reversed : ""}`}>
         <TornBorder top={true} color={colors.greydark1} mobile_view={true}></TornBorder>
         <TornBackgroundSVG className={styles.event_bg}/>
         <div className={styles.content}>
-          <h1 className={`${styles.event_title} ${rubikFont.className}`}>{title}</h1>
+          <h2 className={`${styles.event_title} ${rubikFont.className}`}>{title}</h2>
           <img src={img} className={styles.image} width={500} height={500} alt={alt} />
           <EventIcons {...eventMeta} />
-          <p className={styles.description}>{description}</p>
+          <p 
+            className={`
+              ${styles.description}
+              ${isMobile && !showDescription ? styles.hidden : undefined}  
+            `}>
+              {description}
+          </p>
+          {isMobile && (
+            <button 
+              className={`
+                ${styles.toggle_button} 
+                ${showDescription ? styles.show : undefined}
+              `}
+              onClick={handleDescriptionShow}
+              >
+              <ChevronDownSVG/>
+            </button>
+          )}
         </div>
         <TornBorder top={false} color={colors.greydark1} mobile_view={true}/>
     </div>
