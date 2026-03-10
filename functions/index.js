1e8dabb74cfe95177af4c16b9fa0cb7f17b14197
Titch-dev <lewis.d.perry@gmail.com>
2026-03-10 11:20:32 +0200

    Fixes #43: created cloud function to auto set pool_session status to ended @ 2am

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const autoBump = require("./src/functions/autoBumpMetadata");
 const rollWeekly = require("./src/functions/rollWeeklyEvents");
 const optimiseUploadedImage = require("./src/functions/optimiseUploadedImage");
 const { setUserRole, suspendUser } = require("./src/functions/adminUsers");
+const closePoolSessions = require("./src/functions/closeAllSessionsDaily");
 
 exports.syncFacebookEvents = syncFacebook.syncFacebookEvents;
 exports.syncFacebookEventsNow = syncFacebook.syncFacebookEventsNow;
@@ -15,3 +16,5 @@ exports.optimiseUploadedImage = optimiseUploadedImage.optimiseUploadedImage;
 
 exports.setUserRole = setUserRole;
 exports.suspendUser = suspendUser;
+
+exports.closeAllSessionsDaily = closePoolSessions.closeAllSessionsDaily;
